4c64f1ca0fd6326ab835e6578a29472f974f6f68
dang-git <securefun36@gmail.com>
2018-04-30 22:36:32 +0800

    Merged Jojo and bea changes

--- SMAProject/SMAApp/static/js/dgn1.js
+++ SMAProject/SMAApp/static/js/dgn1.js
@@ -1,10 +1,23 @@
-var sideNav = document.getElementById("sn");
-var tabs = sideNav.getElementsByClassName("tab")
+//var sideNav = document.getElementById("sn");
+//var tabs = sideNav.getElementsByClassName("tab")
 
-for (var i = 0; i < tabs.length; i++) {
-  tabs[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+//for (var i = 0; i < tabs.length; i++) {
+//  tabs[i].addEventListener("click", function() {
+//    var current = document.getElementsByClassName("active");
+//    current[0].className = current[0].className.replace(" active", "");
+//    this.className += " active";
+//  });
+//}
+
+$(document).ready(function(){
+  var pathname = window.location.pathname;
+  
+  pathname = pathname.replace(/\//g, '');
+  
+  $('.tab').removeClass("active");
+  if(pathname == ''){
+      $('.diagnostics').addClass("active")
+  }else{
+      $('.' + pathname).addClass("active");
+  }
+})
