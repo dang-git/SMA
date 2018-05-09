bcf62a2235e01d432ca36ba042fd06e25469306f
Jeoffrey Kent Lama <jeoffrey.lama@pointwest.com.ph>
2018-05-09 16:33:42 +0800

    Format Numbers in influencer page
    The page should display the number of followers and other numbers formatted with comma if necessary

--- SMAProject/SMAApp/static/js/dgn1.js
+++ SMAProject/SMAApp/static/js/dgn1.js
@@ -9,5 +9,17 @@ $(document).ready(function(){
     }else{
         $('.' + pathname).addClass("active");
     }
-}) 
+    
+    addCommaSeparation();
+})
+
+function addCommaSeparation(){
+    $('.commaSeparation').each(function(){
+        var comma = $(this).text();
+        var parts = comma.toString().split(".");
+        parts[0] = parts[0].replace(/\D/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        comma = parts.join(".");
+        $(this).text(comma);
+    });
+}
 
