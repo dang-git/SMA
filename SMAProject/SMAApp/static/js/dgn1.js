1d7ccf31a6f0ec3cd5dd7068106c8e5522565a5d
Jeoffrey Kent Lama <jeoffrey.lama@pointwest.com.ph>
2018-05-09 19:36:12 +0800

    Retain Insights
    Set the insights values from the input boxes to sessions

--- SMAProject/SMAApp/static/js/dgn1.js
+++ SMAProject/SMAApp/static/js/dgn1.js
@@ -11,6 +11,19 @@ $(document).ready(function(){
     }
     
     addCommaSeparation();
+    
+    var searchValue = window.localStorage['search_val'];
+    
+    $('#searchForm').submit(function(){
+        window.localStorage['search_val'] = $("input[name = 'keyword']").val();
+    });
+    
+    if(searchValue == ''){
+        $('#id_keyword').val('');
+    }else{
+        $('#id_keyword').val(window.localStorage['search_val']);
+    }
+    
 })
 
 function addCommaSeparation(){
