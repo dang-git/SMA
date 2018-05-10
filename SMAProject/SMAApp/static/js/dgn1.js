2e0bc34878521f1a54ed47aef3e35123c2d4773e
dang-git <securefun36@gmail.com>
2018-05-10 21:48:24 +0800

    Remove sma.js. Placed loading page on every page. Insights saved on session.

--- SMAProject/SMAApp/static/js/dgn1.js
+++ SMAProject/SMAApp/static/js/dgn1.js
@@ -12,20 +12,30 @@ $(document).ready(function(){
     
     addCommaSeparation();
     
-    var searchValue = window.localStorage['search_val'];
+    // retain search box value
+    var searchValue = window.sessionStorage['search_val'];
     
     $('#searchForm').submit(function(){
-        window.localStorage['search_val'] = $("input[name = 'keyword']").val();
+        window.sessionStorage['search_val'] = $("input[name = 'keyword']").val();
     });
     
     if(searchValue == ''){
         $('#id_keyword').val('');
     }else{
-        $('#id_keyword').val(window.localStorage['search_val']);
+        $('#id_keyword').val(window.sessionStorage['search_val']);
     }
     
+    loadInsightValues();
 })
 
+// display loading screen on search
+$( "#searchButton" ).on( "click", function() {
+    $("#loadingPage").css("display","block");
+    $("#loadingKeyword").text($("#id_keyword").val());
+  });
+
+
+// Adds commas to digit values
 function addCommaSeparation(){
     $('.commaSeparation').each(function(){
         var comma = $(this).text();
@@ -41,6 +51,18 @@ $('#searchButton').on('click', function(){
 // alert("heyo");
 });
 
+// Checks every input box with "search-insights-input" class
+// and returns their values if they have data saved on session
+function loadInsightValues(){
+    $("input").each(function(){
+        var inputId = $(this).attr('id');
+        var inputClass = $(this).attr('class');
+        if(inputClass == "search-insights-input" &&
+           window.sessionStorage[inputId] != null){
+            $(this).val(window.sessionStorage[inputId]);
+        }
+    });
+}
 
 function displayLoadingScreen(){
         $.ajax({
@@ -56,7 +78,8 @@ function updateCount(count){
 
 }
 
-
-
-
-
+// Saves insights to session
+$('.search-insights-input').on('change', function(){
+    var insight = $(this).attr('id');
+    window.sessionStorage[insight] = $(this).val();
+});
--- SMAProject/SMAApp/static/js/sma.js
+++
@@ -1,5 +0,0 @@
-$( "#searchButton" ).on( "click", function() {
-    console.log("heuy");
-    $("#loadingPage").css("display","block");
-    $("#loadingKeyword").text($("#id_keyword").val());
-  });
